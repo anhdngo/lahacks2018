2c807d629e1df9867204ff11096ec930003c85c8
Anh Ngo <adn057@ucsd.edu>
2018-03-31 02:44:34 -0700

    okay i give up what the heck is this

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+//when website is opened
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+//when user queries using text box
 app.post('/foursquare', function(req, res) {
     console.log(req.body.location);
+	
+	var obj = getLatLng();
+	console.log(obj.lat);
+	
 	res.sendFile(__dirname + '/public/index.html');
 });
 
@@ -20,3 +26,17 @@ app.use(express.static('public'));
 app.listen(8000, function() {
     console.log("Listening on localhost:8000");
 });
+
+//returns arbitrary lat and lng, replace with foursquare api
+function getLatLng(){
+	var obj = {
+		lat: 34,
+		lng: 118
+	}
+	return obj
+}
+
+exports.getLatLngData = function(){
+	var obj = getLatLng();
+	return obj
+}
