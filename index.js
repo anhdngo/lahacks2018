c86098f97d8183663b6b3820d70f578caef2dbe1
jennichaau <36621259+jennichaau@users.noreply.github.com>
2018-04-01 04:49:28 -0700

    Updated functionality and improved UI

--- index.js
+++ index.js
@@ -49,19 +49,3 @@ exports.getLatLngData = function(){
 	var obj = getLatLng();
 	return obj
 }
-
-    $(document).ready(function () {
-
-        var makeAllFormSubmitOnEnter = function () {
-            $('form input, form select').live('keypress', function (e) {
-                if (e.which && e.which == 13) {
-                    $(this).parents('form').submit();
-                    return false;
-                } else {
-                    return true;
-                }
-            });
-        };
-
-        makeAllFormSubmitOnEnter();
-    });
