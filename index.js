54af37cdf780d0ede2b288899666630a19df27dd
jennichaau <36621259+jennichaau@users.noreply.github.com>
2018-04-01 02:46:59 -0700

    Improved UI for inputs

--- index.js
+++ index.js
@@ -49,3 +49,19 @@ exports.getLatLngData = function(){
 	var obj = getLatLng();
 	return obj
 }
+
+    $(document).ready(function () {
+
+        var makeAllFormSubmitOnEnter = function () {
+            $('form input, form select').live('keypress', function (e) {
+                if (e.which && e.which == 13) {
+                    $(this).parents('form').submit();
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        };
+
+        makeAllFormSubmitOnEnter();
+    });
