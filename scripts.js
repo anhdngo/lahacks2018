e045ddc6e539f0a76d64e79b67461ebe29cdc4bd
jennichaau <36621259+jennichaau@users.noreply.github.com>
2018-03-31 21:23:14 -0700

    added slideshow

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+
 /* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+toggle between hiding and showing the dropdown content 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -19,3 +35,4 @@ window.onclick = function(event) {
   }
 }
 
+*/
